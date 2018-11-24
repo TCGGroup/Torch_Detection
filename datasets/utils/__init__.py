80d0861916156f4a798aef65507f9fd048a816fc
wsnedy <Jonathan_chen1224@hotmail.com>
2018-11-25 00:00:25 +0800

    Revert "PEP8 check"

--- datasets/utils/__init__.py
+++ datasets/utils/__init__.py
@@ -9,17 +9,14 @@ __all__ = [
     # misc
     'is_str', 'file_is_exist', 'exist_or_mkdir',
     # image
-    'img_read', 'img_write', 'img_visualize', 'img_normalize',
-    'img_denormalize', 'img_resize', 'img_flip', 'img_rotate', 'img_pad',
-    'img_pad_size_divisor', 'img_crop', 'img_aspect_ratio',
-    'img_aspect_ratio_flag',
+    'img_read', 'img_write', 'img_visualize', 'img_normalize', 'img_denormalize',
+    'img_resize', 'img_flip', 'img_rotate', 'img_pad', 'img_pad_size_divisor',
+    'img_crop', 'img_aspect_ratio', 'img_aspect_ratio_flag',
     # bbox
-    'bbox_parse', 'bbox_visualize', 'bbox_normalize', 'bbox_denormalize',
-    'bbox_resize', 'bbox_flip', 'bbox_pad', 'bbox_crop', 'bbox_convert_mode',
-    'bbox_valid',
+    'bbox_parse', 'bbox_visualize', 'bbox_normalize', 'bbox_denormalize', 'bbox_resize',
+    'bbox_flip', 'bbox_pad', 'bbox_crop', 'bbox_convert_mode', 'bbox_valid',
     # mask
-    'mask_parse', 'mask_visualize', 'mask_resize', 'mask_flip', 'mask_crop',
-    'mask_pad',
+    'mask_parse', 'mask_visualize', 'mask_resize', 'mask_flip', 'mask_crop', 'mask_pad',
     # file_handler
     'load', 'dump',
     # data_container
