fff0c874cfe9a7d2f1b3b116053e424b80532ff2
wsnedy <jonathan_chen1224@hotmail.com>
2018-11-30 17:15:27 +0800

    add util functions to concat multiple datasets

---
+++ builder/__init__.py
@@ -0,0 +1,5 @@
+from .utils import *
+
+__all__ = [
+    'obj_from_dict'
+]
--- datasets/concat_datasets.py
+++ datasets/concat_datasets.py
@@ -1,5 +1,7 @@
 import numpy as np
 from torch.utils.data.dataset import ConcatDataset as _ConcatDataset
+from builder import obj_from_dict
+import datasets
 
 
 class ConcatDataset(_ConcatDataset):
@@ -18,3 +20,41 @@ class ConcatDataset(_ConcatDataset):
             for dataset_idx in range(len(self.datasets)):
                 flags.append(self.datasets[dataset_idx].flag)
             self.flag = np.concatenate(flags)
+
+
+def get_datasets(dataset_cfg, parent=datasets, additional_dict=None):
+    if isinstance(dataset_cfg['ann_file'], (tuple, list)):
+        ann_files = dataset_cfg['ann_file']
+        num_dataset = len(ann_files)
+    else:
+        ann_files = [dataset_cfg['ann_file']]
+        num_dataset = 1
+
+    if 'proposal_file' in dataset_cfg:
+        if isinstance(dataset_cfg['proposal_file'], (tuple, list)):
+            proposal_files = dataset_cfg['proposal_file']
+        else:
+            proposal_files = [dataset_cfg['proposal_file']]
+    else:
+        proposal_files = [None] * num_dataset
+    assert len(proposal_files) == num_dataset
+
+    if isinstance(dataset_cfg['img_prefix'], (tuple, list)):
+        img_prefixes = dataset_cfg['img_prefix']
+    else:
+        img_prefixes = [dataset_cfg['img_prefix']] * num_dataset
+    assert len(img_prefixes) == num_dataset
+
+    datasets = []
+    for i in range(num_dataset):
+        args_dict = dataset_cfg.copy()
+        args_dict['ann_file'] = ann_files[i]
+        args_dict['proposal_file'] = proposal_files[i]
+        args_dict['img_prefix'] = img_prefixes[i]
+        dataset = obj_from_dict(args_dict, parent, additional_dict)
+        datasets.append(dataset)
+    if num_dataset > 1:
+        datasets = ConcatDataset(datasets)
+    else:
+        datasets = datasets[0]
+    return datasets
