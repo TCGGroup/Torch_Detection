bc58a84dd28a1410d0393431002c0dc767161427
wsnedy <jonathan_chen1224@hotmail.com>
2018-12-18 23:18:33 +0800

    add module in init files

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,7 +2,8 @@ from .base_dataset import BaseDataset
 from .coco import CocoDataset
 from .voc import VOCDataset
 from .concat_datasets import ConcatDataset, get_datasets
-from .loader import GroupSampler, DistributedGroupSampler
+from .loader import GroupSampler, DistributedGroupSampler, build_dataloader
 
 __all__ = ['BaseDataset', 'CocoDataset', 'VOCDataset', 'ConcatDataset',
-           'get_datasets', 'GroupSampler', 'DistributedGroupSampler']
+           'get_datasets', 'GroupSampler', 'DistributedGroupSampler',
+           'build_dataloader']
--- datasets/loader/__init__.py
+++ datasets/loader/__init__.py
@@ -1,3 +1,6 @@
 from .dataset_sampler import GroupSampler, DistributedGroupSampler
+from .collate import collate
+from .build_dataloader import build_dataloader
 
-__all__ = ['GroupSampler', 'DistributedGroupSampler']
+__all__ = ['GroupSampler', 'DistributedGroupSampler',
+           'collate', 'build_dataloader']
