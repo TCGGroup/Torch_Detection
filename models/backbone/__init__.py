aba7385e365dd6c661262442ed17e65adc76dc70
wsnedy <jonathan_chen1224@hotmail.com>
2018-12-20 20:20:14 +0800

    add ShuffleNetV2 backbone

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,4 +1,5 @@
 from .resnext import ResNeXt
 from .shufflenet import ShuffleNet
+from .shufflenetv2 import ShuffleNetV2
 
-__all__ = ['ResNeXt', 'ShuffleNet']
+__all__ = ['ResNeXt', 'ShuffleNet', 'ShuffleNetV2']
--- models/backbone/shufflenetv2.py
+++ models/backbone/shufflenetv2.py
@@ -7,6 +7,67 @@ from ..utils import conv1x1_group, conv3x3_group, norm_layer, \
     kaiming_init, constant_init, load_checkpoint
 
 
+def InvertedLayer(inplanes,
+                  outplanes,
+                  stride=1,
+                  dilation=1,
+                  use_gn=False,
+                  type='right'):
+    """
+    This layer can be used in ShuffleNetv2.
+
+    The main difference between this layer with InvertedResidual in
+    MobileNetv2 is the use of ReLU layer, in this layer, ReLu layer for
+    depthwise conv is removed.
+
+    There are two types, `left` and `right`, for type `left`,  there are
+    two conv layers: 3x3 dwconv + 1x1 conv, for type `right`,  there are
+    three conv layers: 1x1 conv + 3x3 dwconv + 1x1 conv.
+    """
+    assert type in ['left', 'right']
+    if type == 'left':
+        return nn.Sequential(
+            # dw
+            conv3x3_group(inplanes,
+                          inplanes,
+                          stride=stride,
+                          dilation=dilation,
+                          groups=inplanes),
+            norm_layer(inplanes, use_gn=use_gn),
+            # pw-linear
+            conv1x1_group(inplanes, outplanes),
+            norm_layer(outplanes, use_gn=use_gn),
+            nn.ReLU(inplace=True)
+        )
+    else:
+        return nn.Sequential(
+            # pw
+            conv1x1_group(inplanes, outplanes),
+            norm_layer(outplanes, use_gn=use_gn),
+            nn.ReLU(inplace=True),
+            # dw
+            conv3x3_group(outplanes,
+                          outplanes,
+                          stride=stride,
+                          dilation=dilation,
+                          groups=outplanes),
+            norm_layer(outplanes, use_gn=use_gn),
+            # pw-linear
+            conv1x1_group(outplanes, outplanes),
+            norm_layer(outplanes, use_gn=use_gn),
+            nn.ReLU(inplace=True)
+        )
+
+
+class ChannelSplit(nn.Module):
+    def __init__(self):
+        super(ChannelSplit, self).__init__()
+
+    def forward(self, x):
+        half_channel = x.shape[2] // 2
+        return x[:, :half_channel, ...], x[:, half_channel:, ...]
+
+
 class ShuffleLayer(nn.Module):
     def __init__(self, groups):
         super(ShuffleLayer, self).__init__()
@@ -24,93 +85,66 @@ class ShuffleLayer(nn.Module):
             0, 2, 1, 3, 4).reshape(x.size())
 
 
-class ShuffleNetBottleneck(nn.Module):
-    expansion = 4
+class ShuffleNetv2Bottleneck(nn.Module):
+    expansion = 2
 
     def __init__(self,
                  inplanes,
                  outplanes,
-                 groups,
                  stride=1,
                  dilation=1,
                  use_gn=False,
                  downsample=None):
-        super(ShuffleNetBottleneck, self).__init__()
-
-        # NOTE: Do not use group convolution for the first conv1x1 in Stage 2.
-        g = 1 if inplanes == 24 else groups
+        super(ShuffleNetv2Bottleneck, self).__init__()
+        assert self.expansion == 2
+        assert stride in [1, 2]
         # NOTE: ensure output of concat matches the output channels
         planes = outplanes // self.expansion
-        assert stride in [1, 2]
-        outplanes = outplanes - inplanes if stride == 2 else outplanes
-        self.conv1 = conv1x1_group(inplanes, planes, groups=g)
-        self.shuffle1 = ShuffleLayer(groups=g)
-        self.conv2 = conv3x3_group(
-            planes, planes, stride=stride, dilation=dilation, groups=planes)
-        self.conv3 = conv1x1_group(planes, outplanes, groups=groups)
 
-        # we want to load pre-trained models
-        # for keep the layer name the same as pre-trained models
-        norm_layers = []
-        norm_layers.append(norm_layer(planes, use_gn))
-        norm_layers.append(norm_layer(planes, use_gn))
-        norm_layers.append(norm_layer(outplanes, use_gn))
-        self.norm_names = ['bn1', 'bn2', 'bn3'] \
-            if not use_gn else ['gn1', 'gn2', 'gn3']
-        for name, layer in zip(self.norm_names, norm_layers):
-            self.add_module(name, layer)
-
-        self.relu = nn.ReLU(inplace=True)
+        self.branch = InvertedLayer(inplanes,
+                                    planes,
+                                    stride=stride,
+                                    dilation=dilation,
+                                    use_gn=use_gn,
+                                    type='right')
+        self.channel_split = ChannelSplit()
+        self.shuffle = ShuffleLayer(groups=self.expansion)
         self.downsample = downsample
         self.stride = stride
         self.dilation = dilation
         self.use_gn = use_gn
 
     def forward(self, x):
-        residual = x
+        left, right = self.channel_split(x)
 
-        out = self.conv1(x)
-        norm1 = getattr(self, self.norm_names[0])
-        out = norm1(out)
-        out = self.relu(out)
-        out = self.shuffle1(out)
-
-        out = self.conv2(out)
-        norm2 = getattr(self, self.norm_names[1])
-        out = norm2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        norm3 = getattr(self, self.norm_names[2])
-        out = norm3(out)
-
+        right = self.branch(right)
         if self.stride == 2 and self.downsample is not None:
-            residual = self.downsample(x)
-            out = torch.cat((out, residual), 1)
-        else:
-            out += residual
-
-        out = self.relu(out)
+            left = self.downsample(left)
+        out = torch.cat((left, right), 1)
+        out = self.shuffle(out)
         return out
 
 
-def _make_shuffle_stage(block,
+def _make_shufflev2_stage(block,
                         inplanes,
                         outplanes,
                         blocks,
-                        groups,
                         stride=1,
                         dilation=1,
                         use_gn=False):
     downsample = None
     if stride != 1:
-        downsample = nn.AvgPool2d(kernel_size=3, stride=stride, padding=1)
+        planes = outplanes // block.expansion
+        downsample = InvertedLayer(inplanes,
+                                   planes,
+                                   stride=stride,
+                                   use_gn=use_gn,
+                                   type='left')
 
     layers = []
     layers.append(
         block(inplanes,
               outplanes,
-              groups,
               stride=stride,
               dilation=dilation,
               use_gn=use_gn,
@@ -120,24 +154,25 @@ def _make_shuffle_stage(block,
         layers.append(
             block(inplanes,
                   outplanes,
-                  groups,
                   stride=1,
                   dilation=dilation,
                   use_gn=use_gn))
     return nn.Sequential(*layers)
 
 
-class ShuffleNet(nn.Module):
+class ShuffleNetV2(nn.Module):
     """
-    ShuffleNet backbone.
-    paper: https://arxiv.org/abs/1707.01083
+    ShuffleNetV2 backbone.
+    paper: https://arxiv.org/pdf/1807.11164.pdf
 
-    The main difference between ResNeXt and shuffleNet is using group in the
-    first conv1x1 of a bottleneck, adding shuffle layer after the grouped 1x1
-    conv, changing the group 3x3 conv into depthwise convolution.
+    There are four guidelines in the paper for design efficient and effective
+    model. Referring the paper for details.
+    Use ShuffleNetV2 as a backbone in object detection, for the last stage,
+    we use the last 1x1 conv's output feature maps.
 
     Args:
-        groups (int): Groups used in 1x1 conv, from {1, 2, 3, 4, 8}.
+        width_mult (float): Width multiple times for ShuffleNetv2,
+            from {0.5, 1.0, 1.5, 2.0}.
         num_stages (int): ShuffleNet stages, normally 3.
         strides (Sequence[int]): Strides of the first block of each stage.
         dilations (Sequence[int]): Dilation of each stage.
@@ -153,15 +188,14 @@ class ShuffleNet(nn.Module):
     # the settings for different groups, the format is:
     # groups: (stage_out_planes, stage_num_blocks)
     arch_settings = {
-        1: ((144, 288, 576), (4, 8, 4)),
-        2: ((200, 400, 800), (4, 8, 4)),
-        3: ((240, 480, 960), (4, 8, 4)),
-        4: ((272, 544, 1088), (4, 8, 4)),
-        8: ((384, 768, 1536), (4, 8, 4))
+        0.5: ((48, 96, 192, 1024), (4, 8, 4)),
+        1.0: ((116, 232, 464, 1024), (4, 8, 4)),
+        1.5: ((176, 352, 704, 1024), (4, 8, 4)),
+        2.0: ((244, 488, 976, 2048), (4, 8, 4)),
     }
 
     def __init__(self,
-                 groups,
+                 width_mult,
                  num_stages=3,
                  strides=(2, 2, 2),
                  dilations=(1, 1, 1),
@@ -170,13 +204,13 @@ class ShuffleNet(nn.Module):
                  use_gn=False,
                  bn_eval=True,
                  bn_frozen=False):
-        super(ShuffleNet, self).__init__()
-        if groups not in self.arch_settings:
-            raise KeyError('invalid groups number {} '
-                           'for shuffleNet'.format(groups))
+        super(ShuffleNetV2, self).__init__()
+        if width_mult not in self.arch_settings:
+            raise KeyError('invalid width multiple times {} '
+                           'for shuffleNetV2'.format(width_mult))
 
         assert 1 <= num_stages <= 3
-        stage_outplanes, stage_blocks = self.arch_settings[groups]
+        stage_outplanes, stage_blocks = self.arch_settings[width_mult]
         stage_blocks = stage_blocks[:num_stages]
         assert len(strides) == len(dilations) == num_stages
         assert max(out_indices) < num_stages
@@ -191,8 +225,8 @@ class ShuffleNet(nn.Module):
         self.inplanes = 24
         # stage1
         self.conv1 = conv3x3_group(3, 24, stride=2)
-        self.norm_name = 'bn1' if not use_gn else 'gn1'
-        self.add_module(self.norm_name, norm_layer(24, use_gn))
+        self.norm_name1 = 'bn1' if not use_gn else 'gn1'
+        self.add_module(self.norm_name1, norm_layer(24, use_gn))
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
@@ -202,12 +236,11 @@ class ShuffleNet(nn.Module):
             outplanes = stage_outplanes[i]
             stride = strides[i]
             dilation = dilations[i]
-            shuffle_stage = _make_shuffle_stage(
-                ShuffleNetBottleneck,
+            shuffle_stage = _make_shufflev2_stage(
+                ShuffleNetv2Bottleneck,
                 inplanes=self.inplanes,
                 outplanes=outplanes,
                 blocks=num_blocks,
-                groups=groups,
                 stride=stride,
                 dilation=dilation,
                 use_gn=use_gn)
@@ -216,11 +249,17 @@ class ShuffleNet(nn.Module):
             self.add_module(stage_name, shuffle_stage)
             self.shuffle_stages.append(stage_name)
 
+        # stage5
+        self.conv5 = conv1x1_group(self.inplanes, stage_outplanes[-1])
+        self.norm_name5 = 'bn5' if not use_gn else 'gn5'
+        self.add_module(self.norm_name5,
+                        norm_layer(stage_outplanes[-1], use_gn))
+
         self.feat_dim = stage_outplanes[-1]
 
     def forward(self, x):
         x = self.conv1(x)
-        norm1 = getattr(self, self.norm_name)
+        norm1 = getattr(self, self.norm_name1)
         x = norm1(x)
         x = self.relu(x)
         x = self.maxpool(x)
@@ -229,7 +268,14 @@ class ShuffleNet(nn.Module):
             shuffle_stage = getattr(self, stage_name)
             x = shuffle_stage(x)
             if i in self.out_indices:
-                outs.append(x)
+                if i < 2:
+                    outs.append(x)
+                else:
+                    x = self.conv5(x)
+                    norm5 = getattr(self, self.norm_name5)
+                    x = norm5(x)
+                    x = self.relu(x)
+                    outs.append(x)
         if len(outs) == 1:
             return outs[0]
         else:
@@ -249,7 +295,7 @@ class ShuffleNet(nn.Module):
             raise TypeError('pretrained must be a str or None')
 
     def train(self, mode=True):
-        super(ShuffleNet, self).train(mode)
+        super(ShuffleNetV2, self).train(mode)
         if not self.use_gn:
             if self.bn_eval:
                 for m in self.modules():
@@ -262,14 +308,24 @@ class ShuffleNet(nn.Module):
         if mode and self.frozen_stages >= 0:
             for param in self.conv1.parameters():
                 param.requires_grad = False
-            norm1 = getattr(self, self.norm_name)
+            norm1 = getattr(self, self.norm_name1)
             for param in norm1.parameters():
                 param.requires_grad = False
             norm1.eval()
             norm1.weight.requires_grad = False
             norm1.bias.requires_grad = False
             for i in range(2, self.frozen_stages + 1):
-                mod = getattr(self, 'stage{}'.format(i))
-                mod.eval()
-                for param in mod.parameters():
-                    param.requires_grad = False
+                if i < 5:
+                    mod = getattr(self, 'stage{}'.format(i))
+                    mod.eval()
+                    for param in mod.parameters():
+                        param.requires_grad = False
+                else:
+                    for param in self.conv5.parameters():
+                        param.requires_grad = False
+                    norm5 = getattr(self, self.norm_name5)
+                    for param in norm5.parameters():
+                        param.requires_grad = False
+                    norm5.eval()
+                    norm5.weight.requires_grad = False
+                    norm5.bias.requires_grad = False
