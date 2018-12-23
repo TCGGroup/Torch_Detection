6a0ffee3426f2933a3a0027d841cc7756362b616
maxence <251515635@qq.com>
2018-12-24 00:53:31 +0800

    add conv7x7_group in utils-layer.py

--- models/utils/layers.py
+++ models/utils/layers.py
@@ -29,6 +29,19 @@ def conv3x3_group(in_planes, out_planes, stride=1, dilation=1, groups=1):
                      groups=groups,
                      bias=False)
 
+def conv7x7_group(in_planes, out_planes, stride=1, dilation=1, group=1):
+    """
+    7x7 convolution with padding and group, without bias, in this situation,
+    padding is same as dilation.
+    """
+    return nn.Conv2d(in_channels=in_planes,
+                     out_channels=out_planes,
+                     kernel_size=7,
+                     stride=stride,
+                     padding=dilation,
+                     dilation=dilation,
+                     groups=groups,
+                     bias=False)
 
 def norm_layer(planes, use_gn=False):
     if not use_gn:
